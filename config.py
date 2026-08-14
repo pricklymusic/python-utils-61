52ce53557104e934d0d12bdf16d2d4d4d43497cd
Lukas Gruber <zhnlquxij687@outlook.com>
2026-08-14 16:17:10 +0200

    refactor:type annotations and docstrings added

--- config.py
+++ config.py
@@ -1,24 +1,44 @@
-import json
-import os
+from typing import Dict, Any
 
-class ConfigLoader:
-    def __init__(self, default_config):
-        self.default_config = default_config
-        self.user_config = {}
+class Config:
+    def __init__(self, config_data: Dict[str, Any]) -> None:
+        self._config_data = config_data
 
-    def load(self, file_path):
-        if os.path.isfile(file_path):
-            with open(file_path, 'r') as file:
-                self.user_config = json.load(file)
-        else:
-            self.user_config = {}
+    def get(self, key: str, default: Any = None) -> Any:
+        """
+        Retrieve the value for the specified key.
+        If the key does not exist, return the default value.
+        """
+        return self._config_data.get(key, default)
 
-    def get(self, key, default=None):
-        return self.user_config.get(key, self.default_config.get(key, default))
+    def set(self, key: str, value: Any) -> None:
+        """
+        Set the value for a specified key.
+        If the key already exists, it will be updated.
+        """
+        self._config_data[key] = value
 
-if __name__ == '__main__':
-    default = {'setting1': 'default_value1', 'setting2': 'default_value2'}
-    loader = ConfigLoader(default)
-    loader.load('user_config.json')
-    print(loader.get('setting1'))
-    print(loader.get('setting3', 'fallback_value'))
+    def remove(self, key: str) -> None:
+        """
+        Remove the specified key from the configuration.
+        If the key does not exist, do nothing.
+        """
+        self._config_data.pop(key, None)
+
+    def keys(self) -> list[str]:
+        """
+        Return a list of all keys in the configuration.
+        """
+        return list(self._config_data.keys())
+
+    def values(self) -> list[Any]:
+        """
+        Return a list of all values in the configuration.
+        """
+        return list(self._config_data.values())
+
+    def items(self) -> list[tuple[str, Any]]:
+        """
+        Return a list of all key-value pairs in the configuration.
+        """
+        return list(self._config_data.items())
