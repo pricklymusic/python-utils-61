d421de3fbe320e6ef79ce1df2bc4d806e7bfc31c
Lukas Gruber <zhnlquxij687@outlook.com>
2026-08-14 16:17:51 +0200

    test:logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,19 +1,28 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(name, logfile, level=logging.DEBUG):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    handler = RotatingFileHandler(logfile, maxBytes=5*1024*1024, backupCount=3)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+class CustomLogger:
+    def __init__(self, name, log_file, max_bytes=10**6, backup_count=3):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+        self.handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def log_warning(self, message):
+        self.logger.warning(message)
+
+    def log_error(self, message):
+        self.logger.error(message)
+
+    def log_debug(self, message):
+        self.logger.debug(message)
 
 if __name__ == '__main__':
-    log = setup_logger('my_logger', 'app.log')
-    log.debug('Debug message')
-    log.info('Informational message')
-    log.warning('Warning message')
-    log.error('Error message')
-    log.critical('Critical message')
+    logger = CustomLogger('MyLogger', 'app.log')
+    logger.log_info('This is an info message')
+    logger.log_error('This is an error message')
