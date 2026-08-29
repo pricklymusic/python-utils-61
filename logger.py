d48803b89db4c44018d3714624f83ffb1f7fa984
Lukas Gruber <zhnlquxij687@outlook.com>
2026-08-29 16:11:26 +0200

    feat:setup of rotating file logger

--- logger.py
+++ logger.py
@@ -1,36 +1,42 @@
 import logging
-import sys
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
-class CustomLogger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
+class LoggerSetup:
+    def __init__(self, app_name="utils61", log_file="app.log", max_bytes=5242880, backups=3):
+        self.app_name = app_name
+        self.log_file = Path(log_file)
+        self.max_bytes = max_bytes
+        self.backups = backups
+        self.logger = None
+        self._initialize_logger()
+
+    def _initialize_logger(self):
+        self.log_file.parent.mkdir(parents=True, exist_ok=True)
+        self.logger = logging.getLogger(self.app_name)
         self.logger.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler(sys.stdout)
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+        for h in list(self.logger.handlers):
+            self.logger.removeHandler(h)
+            h.close()
 
-    def debug(self, msg):
-        self.logger.debug(msg)
+        rotating_handler = RotatingFileHandler(
+            str(self.log_file),
+            maxBytes=self.max_bytes,
+            backupCount=self.backups,
+            encoding="utf-8"
+        )
+        rotating_handler.setLevel(logging.INFO)
+        rotating_handler.setFormatter(logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        ))
+        self.logger.addHandler(rotating_handler)
 
-    def info(self, msg):
-        self.logger.info(msg)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.WARNING)
+        stream_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+        self.logger.addHandler(stream_handler)
 
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-
-# Sample usage:
-if __name__ == '__main__':
-    log = CustomLogger('MyLogger')
-    log.debug('This is a debug message')
-    log.info('This is an info message')
-    log.warning('This is a warning message')
-    log.error('This is an error message')
-    log.critical('This is a critical message')
+    def get_logger(self):
+        if self.logger is None:
+            self._initialize_logger()
+        return self.logger
